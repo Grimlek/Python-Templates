f69f20d2d4f28f10f9043418e5819745af04c73a
Charles Sexton <charlie_sexton@yahoo.com>
2017-03-14 10:28:28 -0400

    polish

--- mysql.py
+++ mysql.py
@@ -5,24 +5,24 @@ import csv
 
 conn = pymysql.connect(host='localhost', port=3306, user='root', passwd='password', db='database')
 
-cur = conn.cursor()
-cur.execute("SELECT columns FROM table_nume")
+cursor = conn.cursor()
+cursor.execute("SELECT columns FROM table_name")
 
 names = []
-for row in cur:
+for row in cursor:
     names.append(row[0])
 
 #
-# Possibly need a file to import/export data into/from the database
+# May need a file to import/export data into/from the database
 #
 with open("file_path_tab_delimited.txt") as f:
     reader = csv.reader(f, delimiter="\t") # tab delimited reader
     list = list(reader) # convert file to list, quick solution
 
 
-cur.execute("query")
-print(cur._executed) # print query executed
+cursor.execute("query")
+print(cursor._executed) # print query executed
 
 conn.commit() # commit update/delete/insert/etc.... queries
-cur.close()
+cursor.close()
 conn.close()
